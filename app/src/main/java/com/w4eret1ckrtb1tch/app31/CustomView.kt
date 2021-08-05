2ba841f62dbdc51684e3f6c7b6e1093a09199cf4
ChapaevAnton <virsus2009@gmail.com>
2021-08-05 14:41:03 +0700

    31.5. Canvas

--- app/src/main/java/com/w4eret1ckrtb1tch/app31/CustomView.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/app31/CustomView.kt
@@ -1,9 +1,7 @@
 package com.w4eret1ckrtb1tch.app31
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
@@ -26,7 +24,7 @@ class CustomView @JvmOverloads constructor(context: Context, attr: AttributeSet?
 
         strokePaint = Paint().apply {
             color = strokeColor
-            style = Paint.Style.STROKE
+            style = Paint.Style.FILL_AND_STROKE
             flags = Paint.ANTI_ALIAS_FLAG
             strokeWidth = strokeWidthAttr
         }
@@ -48,9 +46,70 @@ class CustomView @JvmOverloads constructor(context: Context, attr: AttributeSet?
         Log.d("TAG", "onAttachedToWindow: ok")
     }
 
+    //произвольная фигура
+    private val a = Point(0, 0)
+    private val b = Point(0, 100)
+    private val c = Point(87, 50)
+
+    val path = Path().apply {
+        //тип заполнения краской
+        fillType = Path.FillType.EVEN_ODD
+        //Рисуем путь по точкам, созданным ранее
+        lineTo(b.x.toFloat(), b.y.toFloat())
+        lineTo(c.x.toFloat(), c.y.toFloat())
+        lineTo(a.x.toFloat(), a.y.toFloat())
+        close()
+    }
+
+    private val textPath = Path()
+
+    private val textColor = Paint().apply {
+        color = Color.BLUE
+        textSize = 100f
+        typeface = Typeface.SANS_SERIF
+        style = Paint.Style.FILL_AND_STROKE
+        isAntiAlias = true
+
+    }
+
     override fun onDraw(canvas: Canvas?) {
-        canvas?.drawLine(0f, 0f, width.toFloat(), height.toFloat(), strokePaint)
+
+        textPath.addCircle(width.toFloat() / 2, height.toFloat() / 2, 200f,Path.Direction.CW)
+
+        canvas?.let {
+            //текст
+            it.drawCircle(width.toFloat() / 2, height.toFloat() / 2, 200f, getPaintColor(Color.RED))
+            it.drawTextOnPath("CustomView", textPath, 0f, 0f,textColor)
+//            //заливка
+//            it.drawColor(Color.parseColor("#FF5722"))
+//            //линии
+//            it.drawLine(0f, 0f, width.toFloat(), height.toFloat(), strokePaint)
+//            it.drawLine(width.toFloat(), 0f, 0f, height.toFloat(), strokePaint)
+//            //окружность
+//            it.drawCircle(width.toFloat() / 2,height.toFloat()/2,200f,getPaintColor(Color.RED))
+//            //прямоугольник
+//            it.drawRect(
+//                width.toFloat() / 2 - 100f,
+//                height.toFloat() / 2 - 100f,
+//                width.toFloat() / 2 + 100f,
+//                height.toFloat() / 2 + 100f,
+//                getPaintColor(Color.GREEN)
+//            )
+//            //путь
+//            it.drawPath(path,getPaintColor(Color.YELLOW))
+
+
+        }
         Log.d("TAG", "onDraw: ok")
+    }
+
+
+    private fun getPaintColor(strokeColor: Int): Paint {
+        return Paint().apply {
+            color = strokeColor
+            style = Paint.Style.FILL_AND_STROKE
+            flags = Paint.ANTI_ALIAS_FLAG
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
